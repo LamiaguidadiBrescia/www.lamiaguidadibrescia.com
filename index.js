61990b90e3f0369cd9447be397ee066522aefe92
Michele Carnaghi <mcarni@yahoo.it>
2024-01-12 19:55:45 +0100

    setup web structure, routes and initial layout

--- index.js
+++ index.js
@@ -2,58 +2,84 @@
 
 require('dotenv').config();
 
-// require and instantiate express
-//    const app = require('express')()
+// we initialise fs in order to be able to read/write json
+const fs = require('fs');
+let navbar = fs.readFileSync('./data/navbar.json');
+navbar = JSON.parse(navbar);
+let pageNames = fs.readFileSync('./data/pageNames.json');
+pageNames = JSON.parse(pageNames);
+//let titleContent1 = fs.readFileSync('./data/titleContent1.json');
+//titleContent1 = JSON.parse(titleContent1);
+let titleContent1IT = fs.readFileSync('./data/IT/titleContent1.txt');
+let content1IT = fs.readFileSync('./data/IT/content1.txt');
+let content2IT = fs.readFileSync('./data/IT/content2.txt');
+let titleContent2IT = fs.readFileSync('./data/IT/titleContent2.txt');
+let content3IT = fs.readFileSync('./data/IT/content3.txt');
+
+let titleContent1ES = fs.readFileSync('./data/ES/titleContent1.txt');
+let content1ES = fs.readFileSync('./data/ES/content1.txt');
+let content2ES = fs.readFileSync('./data/ES/content2.txt');
+let titleContent2ES = fs.readFileSync('./data/ES/titleContent2.txt');
+let content3ES = fs.readFileSync('./data/ES/content3.txt');
+
+
+let structure = {
+    'navbar': navbar, 
+    'pageNames': pageNames,
+    'content': {
+        'title1': [[titleContent1IT], [titleContent1ES]],
+        'page1': [ [content1IT, content2IT], [content1ES, content2ES]],
+        'title2': [[titleContent2IT], [titleContent2ES]],
+        'page2': [ [content3IT], [content3ES] ]
+        }
+};
+
+/* a bit of logs for debugging ;-)
+console.log('navbar : '+JSON.stringify(navbar, null, 2));
+console.log('pageTitles : '+JSON.stringify(pageTitles, null, 2));
+console.log('content1IT : '+content1IT);
+console.log('content1ES : '+content1ES);
+console.log(structure.content.title1[1]);
+*/
+
+
 // require and instantiate express
 const express = require('express');
+
+// do we need cookieparser, bodyparser and crypto?
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const crypto = require('crypto');
+//const bodyParser = require('body-parser');
+//const crypto = require('crypto');
+
+// instantiate express
 const app = express();
 
-// initializes as a function handler for the HTTP server
-const http = require('http').createServer(app);
+// tell express to use cookieParser
+app.use(cookieParser())
+
+// define local variables
+app.locals.structure = structure;
 
 // define the folder to serve with static content
 app.use(express.static(__dirname + '/'));
 // set the view engine to ejs
 app.set('view engine', 'ejs')
 
-// index page
-app.get('/', function(req, res) {
-  res.render('index', {
-    name : "La mia guida di Brescia",
-    navlink : "home"
-  });
-});
+// let's define different routes for different languages'
+const mainRoute = require('./routes/route.js');
+//const italianRoute = require('./routes/it.js');
+//const spanishRoute = require('./routes/es.js');
+// and tell express to use the defined routes
+app.use(mainRoute);
+//app.use(italianRoute);
+//app.use(spanishRoute);
 
-// games page
-app.get('/games', function(req, res) {
-  res.render('games', {
-    name : "List of Games",
-    navlink : "games"
-  });
-});
-
-// recover page
-app.get('/recover', function(req, res) {
-  res.render('recover', {
-    name : "Recover Cartridges",
-    navlink : "recover"
-  });
-});
-
-// login page
-app.get('/login', function(req, res) {
-  res.render('login', {
-    name : "Login",
-    navlink : "login"
-  });
-});
-
+// initializes as a function handler for the HTTP server
+const http = require('http').createServer(app);
 
 // -------------------------------------------------------------------
 // run your application on PORT specified in env file
 app.listen(process.env.PORT, () => {
     console.log("Server is running on port : " + process.env.PORT)
 });
+
---
+++ routes/route.js
@@ -0,0 +1,159 @@
+/* IT route */
+const express = require("express");
+const router = express.Router();
+
+// home page
+router.get('/', function(req, res) {
+
+  // switch between different languages
+  switch(req.cookies.language) {
+  case "IT":
+    lang = 0;
+    break;
+  case "ES":
+    lang = 1;
+    break;
+  default:
+    lang = 0;
+  }
+  
+  res.render('home', {
+    navlink : 'home',
+    structure : req.app.locals.structure,
+    lang : lang
+  });
+});
+
+// una guida per te
+router.get('/guidaperte', function(req, res) {
+  
+  // switch between different languages
+  switch(req.cookies.language) {
+  case "IT":
+    lang = 0;
+    break;
+  case "ES":
+    lang = 1;
+    break;
+  default:
+    lang = 0;
+  }
+  
+  res.render('guidaperte', {
+    navlink : 'guidaperte',
+    structure : req.app.locals.structure,
+    lang : lang
+  });
+});
+
+// il calendario
+router.get('/calendario', function(req, res) {
+
+  // switch between different languages
+  switch(req.cookies.language) {
+  case "IT":
+    lang = 0;
+    break;
+  case "ES":
+    lang = 1;
+    break;
+  default:
+    lang = 0;
+  }
+  
+  res.render('calendario', {
+    navlink : 'calendario',
+    structure : req.app.locals.structure,
+    lang : lang
+  });
+});
+
+// gli itinerari
+router.get('/itinerari', function(req, res) {
+
+  // switch between different languages
+  switch(req.cookies.language) {
+  case "IT":
+    lang = 0;
+    break;
+  case "ES":
+    lang = 1;
+    break;
+  default:
+    lang = 0;
+  }
+  
+  res.render('itinerari', {
+    navlink : 'itinerari',
+    structure : req.app.locals.structure,
+    lang : lang
+  });
+});
+
+// in evidenza
+router.get('/evidenza', function(req, res) {
+
+  // switch between different languages
+  switch(req.cookies.language) {
+  case "IT":
+    lang = 0;
+    break;
+  case "ES":
+    lang = 1;
+    break;
+  default:
+    lang = 0;
+  }
+  
+  res.render('evidenza', {
+    navlink : 'evidenza',
+    structure : req.app.locals.structure,
+    lang : lang
+  });
+});
+
+// regala una visita
+router.get('/regalavisita', function(req, res) {
+
+  // switch between different languages
+  switch(req.cookies.language) {
+  case "IT":
+    lang = 0;
+    break;
+  case "ES":
+    lang = 1;
+    break;
+  default:
+    lang = 0;
+  }
+  
+  res.render('regalavisita', {
+    navlink : 'regalavisita',
+    structure : req.app.locals.structure,
+    lang : lang
+  });
+});
+
+// contattami
+router.get('/contatti', function(req, res) {
+
+  // switch between different languages
+  switch(req.cookies.language) {
+  case "IT":
+    lang = 0;
+    break;
+  case "ES":
+    lang = 1;
+    break;
+  default:
+    lang = 0;
+  }
+  
+  res.render('contatti', {
+    navlink : 'contatti',
+    structure : req.app.locals.structure,
+    lang : lang
+  });
+});
+
+module.exports = router;
